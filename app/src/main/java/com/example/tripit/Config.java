346c4821c370e332e43e9d459f7688d47da14ac1
shantanuk1998 <shantanu.kurapati@gmail.com>
2020-05-03 17:37:03 +0530

    Sk-mail files added

---
+++ app/src/main/java/com/example/tripit/Config.java
@@ -0,0 +1,5 @@
+package com.example.tripit;
+public class Config {
+    public static final String EMAIL ="shantanu.kurapati.sk@gmail.com";
+    public static final String PASSWORD ="shantanu123";
+}
